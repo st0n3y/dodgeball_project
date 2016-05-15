0b84b99ab7e382148f42114998dffafb0937d268
David MacKintosh <dgawmackintosh@gmail.com>
2016-05-15 13:41:25 +0100

    Works on league table function

--- models/league.rb
+++ models/league.rb
@@ -4,6 +4,42 @@ class League
     @matches = matches
   end
 
-  
+  def total_num_of_matches()
+    return @matches.length
+  end
+
+  def total_points_scored()
+    total = 0
+    @matches.each { |match| total += ( match.home_score + match.away_score ) }
+    return total
+  end
+
+  def num_of_wins( team )
+    total = 0
+    for match in @matches
+      if match.home_id == team.id && match.home_score > match.away_score
+        total += 1
+      elsif match.away_id == team.id && match.away_score > match.home_score
+        total += 1
+      end
+    end
+    return total
+  end
+
+  def num_of_league_points( team )
+    total = 0
+    for match in @matches
+      if match.home_id == team.id && match.home_score > match.away_score
+        total += 2
+      elsif match.away_id == team.id && match.away_score > match.home_score
+            total += 2
+      elsif match.home_id == team.id && match.home_score == match.away_score
+        total += 1
+      elsif match.away_id == team.id && match.away_score == match.home_score
+        total +=1
+      end
+    end
+    return total
+  end
 
 end
--- specs/league_spec.rb
+++ specs/league_spec.rb
@@ -3,14 +3,15 @@ require( 'minitest/rg' )
 require_relative( '../models/league.rb' )
 require_relative( '../models/match.rb' )
 require_relative( '../models/team.rb' )
+require_relative( '../db/sql_runner.rb' )
 
 class TestLeague < MiniTest::Test
 
   def setup()
-    team1 = Team.new( { 'team_name' => 'Radges', 'location' => 'Edinburgh' } )
-    team2 = Team.new( { 'team_name' => 'Bams', 'location' => 'Glasgow' } )
-    team3 = Team.new( { 'team_name' => 'Beanos', 'location' => 'Dundee' } )
-    team4 = Team.new( { 'team_name' => 'Jammy Dodgers', 'location' => 'Inverness' } )
+    t1 = Team.new( { 'id' => 1, 'team_name' => 'Radges', 'location' => 'Edinburgh' } )
+    t2 = Team.new( { 'id' => 2, 'team_name' => 'Bams', 'location' => 'Glasgow' } )
+    t3 = Team.new( { 'id' => 3, 'team_name' => 'Beanos', 'location' => 'Dundee' } )
+    t4 = Team.new( { 'id' => 4, 'team_name' => 'Jammy Dodgers', 'location' => 'Inverness' } )
 
     match1 = Match.new( { 'home_score' => 8, 'away_score' => 5, 'home_id' => t1.id, 'away_id' => t2.id } )
     match2 = Match.new( { 'home_score' => 8, 'away_score' => 4, 'home_id' => t2.id, 'away_id' => t1.id } )
@@ -25,10 +26,50 @@ class TestLeague < MiniTest::Test
     match11 = Match.new( { 'home_score' => 9, 'away_score' => 3, 'home_id' => t3.id, 'away_id' => t4.id } )
     match12 = Match.new( { 'home_score' => 7, 'away_score' => 3, 'home_id' => t4.id, 'away_id' => t3.id } )
 
+    @teams = [ t1, t2, t3, t4 ]
     @matches = [ match1, match2, match3, match4, match5, match6, match7, match8, match9, match10, match11, match12 ]
     @league = League.new( @matches )
   end
 
-  
+  def test_total_num_of_matches()
+    result = @league.total_num_of_matches
+    assert_equal( 12, result )
+  end
+
+  def test_total_points_scored()
+    result = @league.total_points_scored
+    assert_equal( 120, result )
+  end
+
+  def test_num_of_wins_for_t1()
+    result = @league.num_of_wins( @teams[ 0 ] )
+    assert_equal( 3, result )
+  end
+
+  def test_num_of_league_points_for_t3()
+    result = @league.num_of_league_points( @teams[ 2 ] )
+    assert_equal( 8, result )
+  end
+
+  def test_league_table()
+    t1_points = @league.num_of_league_points( @teams[0])
+    t2_points = @league.num_of_league_points( @teams[1])
+    t3_points = @league.num_of_league_points( @teams[2])
+    t4_points = @league.num_of_league_points( @teams[3])
+
+    puts "Team         |  League Points"
+    print "-------------------------------"
+    puts ""
+    puts "Radges         |  " << t1_points
+    print "-------------------------------"
+    puts ""
+    puts "Bams           |  " << t2_points
+    print "-------------------------------"
+    puts ""
+    puts "Beanos         |  " << t3_points
+    print "-------------------------------"
+    puts ""
+    puts "Jammy Dodgers  |  " << t4_points
+  end
 
 end
